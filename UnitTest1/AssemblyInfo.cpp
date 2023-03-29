3d9c1232b4e0a057526e44a9861c920ada6d60fc
Brick Reyes <president@encarga-us.com>
2023-03-29 12:08:20 -0500

    initial commit

---
+++ UnitTest1/AssemblyInfo.cpp
@@ -0,0 +1,34 @@
+#include "stdafx.h"
+
+using namespace System::Reflection;
+using namespace System::Runtime::CompilerServices;
+using namespace System::Runtime::InteropServices;
+
+//
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+//
+[assembly:AssemblyTitleAttribute("UnitTest1")];
+[assembly:AssemblyDescriptionAttribute("")];
+[assembly:AssemblyConfigurationAttribute("")];
+[assembly:AssemblyCompanyAttribute("")];
+[assembly:AssemblyProductAttribute("UnitTest1")];
+[assembly:AssemblyCopyrightAttribute("Copyright (c)  2023")];
+[assembly:AssemblyTrademarkAttribute("")];
+[assembly:AssemblyCultureAttribute("")];
+
+//
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o establecer como predeterminados los números de compilación y de revisión
+// mediante el carácter '*', como se muestra a continuación:
+
+[assembly:AssemblyVersionAttribute("1.0.*")];
+[assembly:ComVisible(false)];
+
